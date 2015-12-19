2028ec6be68906e91bfb0f65bffe2d3613bb8270
action87 <vigh.gabor87@gmail.com>
2015-12-19 08:17:58 +0100

    Adding the class to 5.3.

---
+++ OptionPricer/src/mc/StatisticGatherer.h
@@ -0,0 +1,21 @@
+/*
+ * StatisticGatherer.h
+ *
+ *  Created on: 2015. dec. 15.
+ *      Author: Gabor
+ */
+
+#ifndef MC_STATISTICGATHERER_H_
+#define MC_STATISTICGATHERER_H_
+
+//TODO Implement the exercise 5.3.
+class StatisticGatherer : public StatisticsMC{
+	public:
+
+
+	private:
+
+};
+
+
+#endif /* MC_STATISTICGATHERER_H_ */
